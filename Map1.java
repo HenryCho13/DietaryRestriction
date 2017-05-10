cd8a105f16976fc6a75567a1a4ee7131887dfc99
Sabrina <sabrinama10@gmail.com>
2017-05-10 17:54:06 -0400

    final

--- Map1.java
+++ Map1.java
@@ -11,64 +11,61 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Mapper.Context;
 
-public class Map1 
-	extends Mapper<Object, Text, Text, IntWritable> { 	
+public class Map1 extends Mapper<Object, Text, Text, IntWritable> {
 
 	private Text feelFood = new Text();
 	private Text test = new Text();
 	private final IntWritable one = new IntWritable(1);
-	
-	ArrayList<Food>foodList = new ArrayList<Food>();
 
-	public void map(Object key, Text value, Context context) 
-			throws IOException, InterruptedException {
-		
+	ArrayList<Food> foodList = new ArrayList<Food>();
+
+	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyy/M/d H:mm");
-		String type;	
+		String type;
 		String typeValue;
 		String dateTime;
-	
+
 		String[] row = value.toString().split(" ");
-		
-		type = row[0];
-		typeValue = row[1];
-		dateTime = row[3] + " " + row[4];
-		Date date = null;
-		
-		try {
-			date = formatter.parse(dateTime);
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		
-		if (type.equals("food")) {
-			Food food = new Food(typeValue, date);	//convert date and time to ms 	
-			foodList.add(food);
-		
-		}
-	
-		else if (type.equals("feel")) {
-			for (int i=0; i < foodList.size(); i++) {
-				long diff = date.getTime() - foodList.get(i).getDate().getTime();
-				long diffHours = diff / (60 * 60 * 1000);
-				
-				
-				if (diffHours <= 12) {
-					feelFood.set(typeValue + "\t" + foodList.get(i).getValue());
-					context.write(feelFood, one);
+			type = row[0];
+			typeValue = row[1];
+			dateTime = row[3] + " " + row[4];
+			Date date = null;
+
+			try {
+				date = formatter.parse(dateTime);
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+
+			if (type.equals("food")) {
+				Food food = new Food(typeValue, date); // convert date and time
+														// to ms
+				foodList.add(food);
+
+			}
+
+			else if (type.equals("feel")) {
+				for (int i = 0; i < foodList.size(); i++) {
+					long diff = date.getTime() - foodList.get(i).getDate().getTime();
+					long diffHours = diff / (60 * 60 * 1000);
+
+					if (diffHours <= 12) {
+						feelFood.set(typeValue + "\t" + foodList.get(i).getValue());
+						context.write(feelFood, one);
+					}
+
+//					else {
+//						foodList.remove(i);
+//					}
 				}
-				
-				else {
-					foodList.remove(i);
-	          }
-	        }
-		}
+			}
 	}
-	
+
 	private class Food {
 		String value;
 		Date date;
-		
+
 		Food(String v, Date d) {
 			date = d;
 			value = v;
@@ -91,4 +88,3 @@ public class Map1
 		}
 	}
 }
-
